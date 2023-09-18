a0f8cf7eee1c9f4ca55c94e857bd3b39a71a4e5f
AShrafAbbas98 <ashrafabbasramadan@gmail.com>
2023-09-18 16:15:40 +0300

    2023.09.18

---
+++ StringManipulation/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // #String Manipulation 
+
+            // 01- string.Concat(firstName, " " , secondName);
+
+
+
+            // 02- text.Substring(i) ==> returns part of the string
+
+
+
+            // 03- text.Split(','); ===> split the string whenever catch a "," ==> Returns string[] 
+
+
+
+            // 04- text.Replace("apples", "bananas");
+
+
+
+            // 05- text.ToUpper and text.ToLower()
+
+
+
+            // 06-text.Contains("fox"); ==> Returns bool
+
+
+
+            // 07- String Interpolation ==> $"I am {age} years old." 
+
+
+
+            // 08- String Formatting  ==> string.Format("Name: {0}, Score: {1}", name, score);
+
+
+            //-------------------------------------------------------------------------------------
+
+            // #Parsing
+
+            // string =>  int
+
+
+            // int => string
+
+
+            //-------------------------------------------------------------------------------------
+
+            // #Type Casting
+
+            // 01- implicitly casting
+
+
+            // 01- Explicitly casting
+
+
+            //-------------------------------------------------------------------------------------
+
+            // Array
+
+
+            //-------------------------------------------------------------------------------------
+
+            // #Condition statement
+
+            // if / else
+
+            // else if
+
+            // switch
+
+            // Ternary operator
+
+        }
+    }
+}
